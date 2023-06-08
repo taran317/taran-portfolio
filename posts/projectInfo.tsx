36800ad853b2f33c81ad308ae181c88e6c968634
taran317 <taran34816@gmail.com>
2023-06-08 14:30:36 -0400

    project tags

--- posts/projectInfo.tsx
+++ posts/projectInfo.tsx
@@ -1,5 +1,3 @@
-import * as fs from "fs";
-import path from "path";
 import ProjectProps from "../types/ProjectProps";
 
 const projectInfo: { [key: string]: ProjectProps } = {
@@ -12,32 +10,23 @@ const projectInfo: { [key: string]: ProjectProps } = {
     imageUrl:
       "https://post.healthline.com/wp-content/uploads/2021/03/therapist-and-patient-exposure-therapy-technique-1200x628-facebook.jpg",
     tags: [
-      "React",
-      "TypeScript",
+      "MongoDB",
       "Node.js",
       "Express",
-      "PostgreSQL",
-      "Docker",
-      "AWS",
+      "React",
+      "Material-UI",
+      "Typescript",
     ],
     github: "https://github.com/hack4impact-upenn/exposurepedia",
   },
   mathworks: {
     id: "mathworks",
-    title: "MathWorks (in progress)",
+    title: "MathWorks (current)",
     date: "2021-01-01",
     description:
       "A web app that allows users to search for and learn about the effects of various chemicals on the human body.",
     imageUrl: "https://i.ibb.co/GTxHdt7/membrane.png",
-    tags: [
-      "React",
-      "TypeScript",
-      "Node.js",
-      "Express",
-      "PostgreSQL",
-      "Docker",
-      "AWS",
-    ],
+    tags: ["C++", "MATLAB", "Javascript"],
     github: "https://github.com/hack4impact-upenn/exposurepedia",
   },
   ripple: {
@@ -48,15 +37,7 @@ const projectInfo: { [key: string]: ProjectProps } = {
       "A web app that allows users to search for and learn about the effects of various chemicals on the human body.",
     imageUrl:
       "https://i.ibb.co/M8sFWxS/Screen-Shot-2023-06-07-at-3-24-02-PM.png",
-    tags: [
-      "React",
-      "TypeScript",
-      "Node.js",
-      "Express",
-      "PostgreSQL",
-      "Docker",
-      "AWS",
-    ],
+    tags: ["Swift", "Combine", "SwiftUI", "Django", "PostgreSQL"],
     github: "https://github.com/hack4impact-upenn/exposurepedia",
   },
   "sports-betting": {
@@ -66,15 +47,7 @@ const projectInfo: { [key: string]: ProjectProps } = {
     description:
       "A web app that allows users to search for and learn about the effects of various chemicals on the human body.",
     imageUrl: "https://www.gaming.net/wp-content/uploads/2022/02/nba-bet.jpg",
-    tags: [
-      "React",
-      "TypeScript",
-      "Node.js",
-      "Express",
-      "PostgreSQL",
-      "Docker",
-      "AWS",
-    ],
+    tags: ["MySQL (AWS RDS)", "Node.js", "Express", "React", "Chakra UI"],
     github: "https://github.com/hack4impact-upenn/exposurepedia",
   },
   pennos: {
@@ -85,13 +58,7 @@ const projectInfo: { [key: string]: ProjectProps } = {
       "A web app that allows users to search for and learn about the effects of various chemicals on the human body.",
     imageUrl: "https://i.ibb.co/HK9zWfM/pennos.png",
     tags: [
-      "React",
-      "TypeScript",
-      "Node.js",
-      "Express",
-      "PostgreSQL",
-      "Docker",
-      "AWS",
+        "C",
     ],
     github: "https://github.com/hack4impact-upenn/exposurepedia",
   },
